501e96890eec826d5b5045eef1ecaf47d1736df4
WHometown <saku.holopainen@hotmail.com>
2022-04-22 11:43:14 +0300

    viikko 2 tehtävät ja viikko 3 aloitus

--- perus2/perus2a/script.js
+++ perus2/perus2a/script.js
@@ -33,32 +33,18 @@ else if(luku3 < luku1 && luku3 < luku2) {
 }
 
 function suurin() {
-// muuttujat, arvot
-var luku1 = document.getElementById('luku1').value;
-var luku2 = document.getElementById('luku2').value;
-var luku3 = document.getElementById('luku3').value;
-var luku4 = document.getElementById('luku4').value;
-var luku5 = document.getElementById('luku5').value;
-// tarkistaa, mikä luku on suurin
-  if(luku1 < luku2 && luku3 < luku2 && luku4 < luku2 && luku5 < luku2) {
-    suurin = luku2;
-  }
-  else if(luku1 < luku3 && luku4 < luku3 && luku5 < luku3) {
-    suurin = luku3;
-  }
-  else if(luku1 < luku4 && luku5 < luku4) {
-    suurin = luku4;
-  }
-  else if(luku1 < luku5) {
-    suurin = luku5;
-  }
-  else {
-    suurin = luku1;
-  }
+  var num1 = parseInt(document.getElementById('luku1').value) || 0;
+  var num2 = parseInt(document.getElementById('luku2').value) || 0;
+  var num3 = parseInt(document.getElementById('luku3').value) || 0;
+  var num4 = parseInt(document.getElementById('luku4').value) || 0;
+  var num5 = parseInt(document.getElementById('luku5').value) || 0;
+
+  var max = Math.max(num1, num2, num3, num4, num5);
+
   // ilmoittaa syötetyt luvut
-  alert("Annoit luvut: " + luku1 + ' ' + luku2 + ' ' + luku3 + ' ' + luku4 + ' ' + luku5);
+  alert("Annoit luvut: " + num1 + ' ' + num2 + ' ' + num3 + ' ' + num4 + ' ' + num5);
   //ilmoittaa suurimman luvun
-  alert("Suurin: " + suurin);
+  alert("Suurin: " + max);
 }
 function pari() {
 // muuttuja, arvo
@@ -76,7 +62,7 @@ function ajoneuvos() {
 // jne jne
 var ika = document.getElementById('ika').value;
 // tarkistaa iän
-  if (ika < 15) {
+  if (ika < 16) {
     alert("Voit ajaa polkupyörällä");
  }
   else if (ika < 18) {
--- perus2/perus2b/script.js
+++ perus2/perus2b/script.js
@@ -1,15 +1,21 @@
 function plusMiinus() {
+//esitellään muuttuja jonka arvo saadaan html:stä
 var luku = document.getElementById('luku').value;
+//tarkastetaan onko luku suurempi tai yhtä suuri kuin 0
   if (luku >= 0) {
+    //ilmoitetaan ilmoitusruudulla, että luku on suurempi tai yhtä suuri kuin 0
     alert("Luku on positiivinen");
 }
   else {
+    //ilmoitetaan ilmoitusruudulla, että luku on pienempi kuin 0
     alert("Luku on negatiivinen");
   }
 }
 
 function mikaPaiva() {
+  //esitellään muuttuja jonka arvo saadaan html:stä
 var day = parseInt(document.getElementById('paiva').value);
+  //Jos muuttujan arvo on 1-7, ilmoitetaan erillisellä ilmoitusruudulla vastaava viikonpäivä
   switch (day) {
     case 1:
     alert("Maanantai");
@@ -29,17 +35,26 @@ var day = parseInt(document.getElementById('paiva').value);
     case 6:
     alert("Lauantai");
     break;
-    default:
+    case 7:
     alert("Sunnuntai");
+    break;
+    //ilmoitetaan, jos annettu arvo ei ole 1-7
+    default:
+    alert("Syötä arvo välillä 1-7");
     break;
   }
 }
 
 function karkaus() {
+//esitellään muuttuja jonka arvo saadaan html:stä
 var vuosi = document.getElementById('vuosi').value;
+//Tarkistetaan onko luku jaollinen neljällä
 if(vuosi % 4 == 0) {
+   //Tarkistetaan onko luku jaollinen sadalla
     if(vuosi % 100 == 0) {
+      //Tarkistetaan onko luku jaollinen neljälläsadalla
        if(vuosi % 400 == 0) {
+         //Tulostetaan erilliselle ilmoitusruudulle
           alert("Vuosi on karkausvuosi");
        }
        else {
@@ -56,29 +71,31 @@ if(vuosi % 4 == 0) {
 }
 
 function summaKarvo() {
+//esitellään muuttujat joiden arvot saadaan html:stä
 var l1, l2, l3, l4, l5;
   l1 = parseInt(document.getElementById('l1').value);
   l2 = parseInt(document.getElementById('l2').value);
   l3 = parseInt(document.getElementById('l3').value);
   l4 = parseInt(document.getElementById('l4').value);
   l5 = parseInt(document.getElementById('l5').value);
+// esitellään muuttuja ja annetaan arvoksi aiemmin esiteltyjen muuttujien summa
 var summa = l1 + l2 + l3 + l4 + l5;
+//esitellään muuttuja jonka arvo on aiemmin esiteltyjen muuttujien keskiarvo
 var karvo = summa / 5
+//Tulostetaan erilliselle ilmoitusruudulle annettujen lukujen summa ja kerskiarvo
 alert('summa on '+summa+' ja keskiarvo on '+karvo);
 }
-//???
-var luku2 = parseInt(document.getElementById('luku2').value);
-var lause
-function lauseMuuttuja() {
-  lause += luku2 + "x 1 = " + luku2 * 1;
-  lause += luku2 + "x 2 = " + luku2 * 2;
-  lause += luku2 + "x 3 = " + luku2 * 3;
-  lause += luku2 + "x 4 = " + luku2 * 4;
-  lause += luku2 + "x 5 = " + luku2 * 5;
-  lause += luku2 + "x 6 = " + luku2 * 6;
-  lause += luku2 + "x 7 = " + luku2 * 7;
-  lause += luku2 + "x 8 = " + luku2 * 8;
-  lause += luku2 + "x 9 = " + luku2 * 9;
-  lause += luku2 + "x 10 = " + luku2 * 10;
-  alert(lause);
+
+function kerto() {
+  //esitellään muuttuja ja annetaan sille alkuarvo; tyhjä merkkijono
+  var tuloste = "";
+  //esitellään muuttuja ja annetaan sille alkuarvo, joka saadaan html:stä
+  var luku2 = parseInt(document.getElementById('luku2').value);
+  //Esitellään muuttuja, joka saa silmukassa arvon 1-10
+    for (var i = 1; i <= 10; i++) {
+      //Lisätään tulostettavaan merkkijonoon kertolasku ja tulos
+      tuloste += luku2 + "*" + i + "=" + luku2 * i + "     ";
+    }
+    //Tulostetaan erilliselle ilmoitusruudulle annetun luvun kymmenen kertotaulu
+    alert('Kymmenen kertotaulu luvulle ' + luku2 + "     " + tuloste);
 }
